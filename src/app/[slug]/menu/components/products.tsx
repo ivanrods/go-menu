639e7152261cff27a3e33990aa61465b568a2dc5
Ivan <contaivanrodrigues@gmail.com>
2025-02-21 14:58:06 -0300

    feat: add product details page

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -3,19 +3,20 @@ import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
+import { formatCurrency } from "@/helpers/format-currency";
+
 interface ProductsProps {
   products: Product[];
 }
 
 const Products = ({ products }: ProductsProps) => {
-  const {slug} = useParams<{slug: string}>()
+  const { slug } = useParams<{ slug: string }>();
   return (
     <div className="space-y-3 px-5">
       {products.map((product) => (
         <Link
-        href={`/${slug}/menu/${product.id}`}
+          href={`/${slug}/menu/${product.id}`}
           key={product.id}
-    
           className="flex items-center justify-between gap-10 border-b py-3"
         >
           {/* ESQUERDA */}
@@ -25,10 +26,7 @@ const Products = ({ products }: ProductsProps) => {
               {product.description}
             </p>
             <p className="pt-3 text-sm font-semibold">
-              {new Intl.NumberFormat("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              }).format(product.price)}
+              {formatCurrency(product.price)}
             </p>
           </div>
 
